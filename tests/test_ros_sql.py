fc384a4d7049a6110daf142b29453b81b445efe4
Andrew Straw <strawman@astraw.com>
2012-09-03 00:36:20 +0200

    add an all-in-one tough test

--- tests/test_ros_sql.py
+++ tests/test_ros_sql.py
@@ -3,6 +3,7 @@ import sqlalchemy
 
 import roslib
 roslib.load_manifest('ros_sql')
+import ros_sql.msg
 import ros_sql.ros2sql as ros2sql
 
 import std_msgs.msg
@@ -28,6 +29,7 @@ def reset_sqlalchemy():
 
     # connect to in-memory db
     elixir.metadata.bind = "sqlite:///:memory:"
+    #elixir.metadata.bind.echo = True
 
 def test_simple_message_roundtrip():
     pose1 = geometry_msgs.msg.Pose()
@@ -40,6 +42,16 @@ def test_simple_message_roundtrip():
     pose1.orientation.z = 0
     pose1.orientation.w = 4.56
 
+    pose2 = geometry_msgs.msg.Pose()
+    pose2.position.x = 2
+    pose2.position.y = 3
+    pose2.position.z = 4
+
+    pose2.orientation.x = -0.02
+    pose2.orientation.y = 1
+    pose2.orientation.z = 3
+    pose2.orientation.w = 5.67
+
     header = std_msgs.msg.Header()
     header.seq = 938
     header.stamp.secs = 3928
@@ -49,13 +61,25 @@ def test_simple_message_roundtrip():
     pa1 = geometry_msgs.msg.PoseArray()
     pa1.header = header
     pa1.poses.append( pose1 )
+    pa1.poses.append( pose2 )
+
+    tc = ros_sql.msg.TestComplex()
+    tc.pa = pa1
+    tc.string_array = map( std_msgs.msg.String, [ 'here', 'are', 'some', '\0complex', 'strings' ])
+    tc.uint8_array = map( std_msgs.msg.UInt8, [  0, 3, 254 ])
+    tc.header = header
+
+    # These are all-but-one disabled because, for now, we save schema
+    # a module and import it. So we can test only one
+    # schema-generation run. Thus we choose a complex one.
 
     for tn,mc,md in [#('/test_string',std_msgs.msg.String, std_msgs.msg.String('xyz')),
                      #('/test_int8', std_msgs.msg.Int8, std_msgs.msg.Int8(-4)),
                      #('/test_uint8', std_msgs.msg.UInt8, std_msgs.msg.UInt8(254)),
                      #('/test_pose', geometry_msgs.msg.Pose, pose1),
                      #('/myheader', std_msgs.msg.Header, header),
-                     ('/test_pose_array', geometry_msgs.msg.PoseArray, pa1),
+                     #('/test_pose_array', geometry_msgs.msg.PoseArray, pa1),
+                     ('/test_complex', ros_sql.msg.TestComplex, tc),
                      ]:
         yield check_roundtrip, tn,mc,md
 
@@ -92,4 +116,11 @@ def check_roundtrip( topic_name, msg_class, msg_expected ):
     timestamp = result['timestamp']
     msg_actual = result['msg']
 
+    print 'FINAL *************'
+    print
+    print 'msg_expected'
+    print msg_expected
+    print
+    print 'msg_actual'
+    print msg_actual
     assert msg_actual == msg_expected
