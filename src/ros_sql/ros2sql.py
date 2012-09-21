a3580442cc73953d3dd4e94b8bd273bb72f718de
Andrew Straw <strawman@astraw.com>
2012-09-21 02:37:23 +0200

    refactor: improve code layout and add docstrings

--- src/ros_sql/ros2sql.py
+++ src/ros_sql/ros2sql.py
@@ -16,87 +16,55 @@ from ros_sql.models import ROS_SQL_COLNAME_PREFIX, \
      RosSqlMetadata, RosSqlMetadataBackrefs, RosSqlMetadataTimestamps
 import ros_sql.util as util
 
-class MetadataChangedError(RuntimeError):
-    def __init__(self,old_meta_row, new_meta_row):
-        super(MetadataChangedError,self).__init__(
-            'metadata changed (original topic %r, new topic %r)'%
-            (old_meta_row.topic_name, new_meta_row.topic_name))
-        self.old_meta_row = old_meta_row
-        self.new_meta_row = new_meta_row
+def add_schemas( session, metadata, list_of_topics_and_messages ):
+    """create tables for multiple topics and messages"""
+    for topic_name, msg_class in list_of_topics_and_messages:
+        gen_schema( session, metadata, topic_name, msg_class )
 
-def parse_field( session, metadata, topic_name, _type, source_topic_name, field_name,
-                 parent_pk_name, parent_pk_type, parent_table_name ):
-    """for a given element within a message, find the schema field type"""
-    dt = type_map.get(_type,None)
-    tab_track_rows = []
+def gen_schema( session, metadata, topic_name, msg_class):
+    """create a SQL table(s) for a given topic and message class"""
+    # add table(s) to MetaData instance
+    rx = generate_schema_raw(session, metadata,topic_name,msg_class, top=True)
+    metadata.create_all( metadata.bind )
 
-    if dt is not None:
-        # simple type
-        results = {#'assign':dt,
-                   'tab_track_rows':tab_track_rows,
-                   'col_args': (),
-                   'col_kwargs': {'type_':dt},
-                   'backref_info_list':[],
-                   }
-        return results
+    # add table tracking row to MetaData instance
+    Base.metadata.create_all( metadata.bind )
 
-    other_instance_name = util.namify( topic_name )
+    for new_meta_row_args in rx['tracking_table_rows']:
+        args = new_meta_row_args['row_args']
+        newts_rows = []
+        backref_rows = []
+        for ts_row,is_duration in new_meta_row_args['timestamp_colnames']:
+            newts_row = RosSqlMetadataTimestamps(ts_row,is_duration)
+            newts_rows.append(newts_row)
+        for bi in new_meta_row_args['backref_info_list']:
+            backref_row = RosSqlMetadataBackrefs( bi['parent_field'],
+                                                  bi['child_table'],
+                                                  bi['child_field'],
+                                                  )
+            backref_rows.append( backref_row )
+        new_meta_row = RosSqlMetadata(*args)
+        new_meta_row.timestamps = newts_rows
+        new_meta_row.backrefs = backref_rows
 
-    if _type.endswith('[]'):
-        # array - need to start another sql table
-        element_type_name = _type[:-2]
-        dt = type_map.get(element_type_name,None)
-        backref_info_list = []
-
-        if dt is not None:
-            # array of fundamental type
-            element_class_name = util.slot_type_to_class_name(element_type_name)
-            msg_class = getattr(std_msgs.msg,element_class_name)
-            known_sql_type=dt
+        # if equivalent is already in database, don't add
+        old_meta_rows=session.query(RosSqlMetadata).filter_by(topic_name=new_meta_row.topic_name).all()
+        if len(old_meta_rows):
+            assert len(old_meta_rows)==1
+            old_meta_row = old_meta_rows[0]
+            if not old_meta_row.is_equal(new_meta_row):
+                raise MetadataChangedError(old_meta_row, new_meta_row)
         else:
-            # array of non-fundamental type
-            msg_class = roslib.message.get_message_class(element_type_name)
-            known_sql_type=None
+            # this metadata is not already present - add it
+            session.add(new_meta_row)
 
-        rx = generate_schema_raw(session, metadata,
-                                 topic_name, msg_class, top=False,
-                                 known_sql_type=known_sql_type,
-                                 many_to_one=(parent_table_name,parent_pk_name,parent_pk_type),
-                                 )
-        bi = {'parent_field':field_name,
-              'child_table':rx['table_name'],
-              'child_field':rx['foreign_key_column_name'],
-              }
-        backref_info_list.append( bi )
-
-        tab_track_rows.extend( rx['tracking_table_rows'] )
-        other_key_name = other_instance_name + '.' + rx['pk_name']
-        results = {
-            'tab_track_rows':tab_track_rows,
-            'backref_info_list':backref_info_list,
-                   }
-    else:
-        # _type is another message type
-        msg_class = roslib.message.get_message_class(_type)
-        rx = generate_schema_raw(session, metadata,topic_name,msg_class, top=False)
-        tab_track_rows.extend( rx['tracking_table_rows'] )
-        other_key_name = other_instance_name + '.' + rx['pk_name']
-
-        results = {
-            'col_args': ( sqlalchemy.ForeignKey(other_key_name,ondelete='cascade'), ),
-            'col_kwargs': {'type_':rx['pk_type'],
-                           'nullable':False,
-                           },
-            'tab_track_rows':tab_track_rows,
-            'backref_info_list':[],
-                   }
-    return results
+    session.commit()
 
 def generate_schema_raw( session, metadata,
                          topic_name, msg_class, top=True,
                          many_to_one=None,
                          known_sql_type=None):
-    """convert a message type into a Python source code string"""
+    """convert a message type into an SQL database schema"""
     tracking_table_rows = []
     timestamp_columns = []
     backref_info_list = []
@@ -181,44 +149,78 @@ def generate_schema_raw( session, metadata,
         results['foreign_key_column_name']=foreign_key_column_name
     return results
 
-def gen_schema( session, metadata, topic_name, msg_class):
-    # add table(s) to MetaData instance
-    rx = generate_schema_raw(session, metadata,topic_name,msg_class, top=True)
-    metadata.create_all( metadata.bind )
+def parse_field( session, metadata, topic_name, _type, source_topic_name, field_name,
+                 parent_pk_name, parent_pk_type, parent_table_name ):
+    """for a given element within a message, find the schema field type"""
+    dt = type_map.get(_type,None)
+    tab_track_rows = []
 
-    # add table tracking row to MetaData instance
-    Base.metadata.create_all( metadata.bind )
+    if dt is not None:
+        # simple type
+        results = {#'assign':dt,
+                   'tab_track_rows':tab_track_rows,
+                   'col_args': (),
+                   'col_kwargs': {'type_':dt},
+                   'backref_info_list':[],
+                   }
+        return results
 
-    for new_meta_row_args in rx['tracking_table_rows']:
-        args = new_meta_row_args['row_args']
-        newts_rows = []
-        backref_rows = []
-        for ts_row,is_duration in new_meta_row_args['timestamp_colnames']:
-            newts_row = RosSqlMetadataTimestamps(ts_row,is_duration)
-            newts_rows.append(newts_row)
-        for bi in new_meta_row_args['backref_info_list']:
-            backref_row = RosSqlMetadataBackrefs( bi['parent_field'],
-                                                  bi['child_table'],
-                                                  bi['child_field'],
-                                                  )
-            backref_rows.append( backref_row )
-        new_meta_row = RosSqlMetadata(*args)
-        new_meta_row.timestamps = newts_rows
-        new_meta_row.backrefs = backref_rows
+    other_instance_name = util.namify( topic_name )
 
-        # if equivalent is already in database, don't add
-        old_meta_rows=session.query(RosSqlMetadata).filter_by(topic_name=new_meta_row.topic_name).all()
-        if len(old_meta_rows):
-            assert len(old_meta_rows)==1
-            old_meta_row = old_meta_rows[0]
-            if not old_meta_row.is_equal(new_meta_row):
-                raise MetadataChangedError(old_meta_row, new_meta_row)
+    if _type.endswith('[]'):
+        # array - need to start another sql table
+        element_type_name = _type[:-2]
+        dt = type_map.get(element_type_name,None)
+        backref_info_list = []
+
+        if dt is not None:
+            # array of fundamental type
+            element_class_name = util.slot_type_to_class_name(element_type_name)
+            msg_class = getattr(std_msgs.msg,element_class_name)
+            known_sql_type=dt
         else:
-            # this metadata is not already present - add it
-            session.add(new_meta_row)
+            # array of non-fundamental type
+            msg_class = roslib.message.get_message_class(element_type_name)
+            known_sql_type=None
 
-    session.commit()
+        rx = generate_schema_raw(session, metadata,
+                                 topic_name, msg_class, top=False,
+                                 known_sql_type=known_sql_type,
+                                 many_to_one=(parent_table_name,parent_pk_name,parent_pk_type),
+                                 )
+        bi = {'parent_field':field_name,
+              'child_table':rx['table_name'],
+              'child_field':rx['foreign_key_column_name'],
+              }
+        backref_info_list.append( bi )
 
-def add_schemas( session, metadata, list_of_topics_and_messages ):
-    for topic_name, msg_class in list_of_topics_and_messages:
-        gen_schema( session, metadata, topic_name, msg_class )
+        tab_track_rows.extend( rx['tracking_table_rows'] )
+        other_key_name = other_instance_name + '.' + rx['pk_name']
+        results = {
+            'tab_track_rows':tab_track_rows,
+            'backref_info_list':backref_info_list,
+                   }
+    else:
+        # _type is another message type
+        msg_class = roslib.message.get_message_class(_type)
+        rx = generate_schema_raw(session, metadata,topic_name,msg_class, top=False)
+        tab_track_rows.extend( rx['tracking_table_rows'] )
+        other_key_name = other_instance_name + '.' + rx['pk_name']
+
+        results = {
+            'col_args': ( sqlalchemy.ForeignKey(other_key_name,ondelete='cascade'), ),
+            'col_kwargs': {'type_':rx['pk_type'],
+                           'nullable':False,
+                           },
+            'tab_track_rows':tab_track_rows,
+            'backref_info_list':[],
+                   }
+    return results
+
+class MetadataChangedError(RuntimeError):
+    def __init__(self,old_meta_row, new_meta_row):
+        super(MetadataChangedError,self).__init__(
+            'metadata changed (original topic %r, new topic %r)'%
+            (old_meta_row.topic_name, new_meta_row.topic_name))
+        self.old_meta_row = old_meta_row
+        self.new_meta_row = new_meta_row
