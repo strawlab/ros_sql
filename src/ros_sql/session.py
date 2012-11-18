8952305379d284141772c354b38ba0601493e9e6
Andrew Straw <strawman@astraw.com>
2012-11-18 14:31:55 +0100

    session: use scoped_session()

--- src/ros_sql/session.py
+++ src/ros_sql/session.py
@@ -1,8 +1,10 @@
-import sqlalchemy
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 def get_session(metadata):
     # This is likely not 100% sqlalchemy best practices compliant, but
     # at least it keeps the session instantiation all in one place.
-    Session = sqlalchemy.orm.sessionmaker(bind=metadata.bind)
+    session_factory = sessionmaker(bind=metadata.bind)
+    Session = scoped_session(session_factory)
     session = Session()
     return session
