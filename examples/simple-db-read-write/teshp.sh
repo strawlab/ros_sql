d7f7dfbbfc23228937647586b31c85f450ce99a7
John Stowers <john.stowers@gmail.com>
2014-02-21 17:15:22 +0100

    test scrips

---
+++ examples/simple-db-read-write/teshp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#rostopic pub /nice std_msgs/String indeed -r 100 __name:=pub1 &
+rostopic pub /test std_msgs/Int32 5 -r 10 __name:=pub2 &
+
+rm -f dbname
+
+rosrun ros_sql ros_sql record --bind sqlite:///dbname --all --prefix foo __name:=reca &
+sleep 5
+rosnode kill reca
+
+rosnode kill pub1
+rosnode kill pub2
+sleep 1
